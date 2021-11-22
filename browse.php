34685a6a512861d0792f2b0a011792a3240436e6
alyssa <23alyssa23@gmail.com>
2021-11-22 12:37:14 -0800

    utilities folder, db connect

--- browse.php
+++ browse.php
@@ -1,7 +1,35 @@
 <?php
 
-include('header.php');
+//display all errors on screen
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-include('footer.php');
 
+// Import the db configuration file here and create a connection to the DB
+require('private/db.php');
+$connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
+// Make sure the connection is successfully established, otherwise stop processing the rest of the script.
+if(mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    exit();
+  }
+
+
+//header.php has information for bootstrap and page title
+require('utilities/header.php');
+
+//functions.php contains all the helper functions to make the form and display the table
+// require('functions.php');
+
+?>
+
+
+
+
+<?php 
+
+//page footer for bootstrap and copyright
+require('utilities/footer.php');
 ?>
---
+++ utilities/db.php
@@ -0,0 +1,9 @@
+<?php 
+
+//connect via root login and localhost
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "iat352_project";
+
+?>
