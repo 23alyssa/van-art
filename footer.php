59b2a536f54b62db8c9f2d1dab09649da9884387
alyssa <23alyssa23@gmail.com>
2021-11-22 12:28:26 -0800

    Created footer.php and started browse.php

---
+++ footer.php
@@ -0,0 +1,58 @@
+<section class="">
+  <!-- Footer -->
+  <footer class="bg-secondary text-white text-center text-md-start">
+    <!-- Grid container -->
+    <div class="container p-4">
+      <!--Grid row-->
+      <div class="row">
+        <!--Grid column-->
+        <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
+          <h5 class="text-uppercase">Vancouver Public Art</h5>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
+            molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae
+            aliquam voluptatem veniam, est atque cumque eum delectus sint!
+          </p>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase mb-0">Links</h5>
+
+          <ul class="list-unstyled">
+            <li>
+              <a href="#!" class="text-white">Home</a>
+            </li>
+            <li>
+              <a href="#!" class="text-white">Browse</a>
+            </li>
+            <li>
+              <a href="#!" class="text-white">Login</a>
+            </li>
+            <li>
+              <a href="#!" class="text-white">Sign up</a>
+            </li>
+          </ul>
+        </div>
+        <!--Grid column-->
+      </div>
+      <!--Grid row-->
+    </div>
+    <!-- Grid container -->
+
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+      © 2021 Copyright: Alyssa Lalani & Derek Liu
+    </div>
+    <!-- Copyright -->
+  </footer>
+  <!-- Footer -->
+</section>
