a9a2dfd0f0ee5595282748d18cb99fc398ef958f
derek <derekliu26@gmail.com>
2021-11-23 21:16:59 -0800

    push login, register, logout and private folder

---
+++ login.php
@@ -0,0 +1,68 @@
+<?php
+require_once('private/initialize.php');
+
+$errors = [];
+$username = '';
+$password = '';
+
+// TODO: This page should not show if a session is present.
+// Redirect to staff index if a session is detected.
+if(isset($_SESSION['username'])) {redirect_to(url_for('van-art/browse.php'));}
+
+
+// END TODO
+if(is_post_request()) {
+  // TODO: Verify the password matches the record
+  // if it does not, throw an error message
+  // otherwise set the session and redirect to dashboard
+  if(!empty($_POST['username']) && !empty($_POST['password'])) {
+    // Write a query to retrieve the hashed_password
+    $user_query = "SELECT hashed_password FROM member WHERE username = '" . $_POST['username'] . "'";
+    $user_res = mysqli_query($db, $user_query);
+
+    // If there is no record, then it should just display the error message
+     if(mysqli_num_rows($user_res) != 0) {
+      // Save the hashed password from db into a variable
+        $hashed_password = mysqli_fetch_assoc($user_res)['hashed_password'];
+
+        // Use password verify to check if the entered password matches
+        if(password_verify($_POST['password'], $hashed_password)) {
+
+          // Store session and redirect
+          $_SESSION['username'] = $_POST['username'];
+          redirect_to(url_for('/van-art/browse.php'));
+        } else {
+          // If verify fails, display an error message
+          array_push($errors, "The entered password do not match our record.");
+        }
+      } else  {
+        array_push($errors, "The account does not exist.");
+      }
+  } else {
+    array_push($errors, "Username or password field is not filled.");
+  }
+
+  // END TODO
+}
+
+?>
+
+<?php $page_title = 'Log in'; ?>
+<?php include(SHARED_PATH . '/../utilities/header.php'); ?>
+
+<div id="content">
+  <h1>Log in</h1>
+
+  <?php echo display_errors($errors); ?>
+
+  <form action="login.php" method="post">
+    Username:<br />
+    <input type="text" name="username" value="" /><br />
+    Password:<br />
+    <input type="password" name="password" value="" /><br />
+    <input type="submit" />
+  </form>
+
+</div>
+
+<?php include(SHARED_PATH . '/../utilities/footer.php'); ?>
---
+++ register.php
@@ -0,0 +1,80 @@
+<?php
+require_once('private/initialize.php');
+
+$errors = [];
+
+// TODO: This page should not show if a session is present.
+// Redirect to staff index if a session is detected.
+// if(isset($_SESSION['username'])) {redirect_to(url_for('van-art/browse.php'));}
+
+// END TODO
+if(is_post_request()) {
+  // TODO: check for existing user account, if there is none, encrypt the password and save the entry
+  // Make sure password matches
+  // After the entry is inserted successfully, redirect to dashboard page
+
+  // Check password match first to save time
+  if($_POST['password'] === $_POST['password_confirm']) {
+    // If password matches, check for existing user
+    $existing_query = "SELECT COUNT(*) as count FROM member WHERE username = '" . $_POST['username'] . "'";
+    $existing_res = mysqli_query($db, $existing_query);
+
+    // If the count is not 0, that means an account with the same username already exists
+    if(mysqli_fetch_assoc($existing_res)['count'] != 0) {
+      array_push($errors, 'The username already exists in the database, please try another username instead');
+    } else {
+      // Else encrpyt the password
+      $hashed_password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+      $insert_user_query = "INSERT INTO member(username, email, hashed_password, first_name, last_name) VALUES (
+                            '" . mysqli_real_escape_string($db, $_POST['username'])  . "',
+                            '" . mysqli_real_escape_string($db, $_POST['email']) . "',
+                            '" . mysqli_real_escape_string($db, $hashed_password) . "',
+                            '" . mysqli_real_escape_string($db, $_POST['first_name']) . "',
+                            '" . mysqli_real_escape_string($db, $_POST['last_name']) . "')";
+
+      if(mysqli_query($db, $insert_user_query)) {
+        // INSERT is successful, save a session then redirect to dashboard
+        $_SESSION['username'] = $_POST['username'];
+        redirect_to(url_for('van-art/browse.php'));
+      } else {
+        // Display the mysql error if failed
+        array_push($errors, mysqli_error($db));
+      }
+    }
+  } else {
+    array_push($errors, 'Password do not match');
+  }
+
+
+  // END TODO
+}
+
+?>
+
+<?php $page_title = 'Register'; ?>
+<?php include(SHARED_PATH . '/../utilities/header.php'); ?>
+
+<div id="content">
+  <h1>Register</h1>
+
+  <?php echo display_errors($errors); ?>
+
+  <form action="register.php" method="post">
+    First Name:<br />
+    <input type="text" name="first_name" value="" required /><br />
+    Last Name:<br />
+    <input type="text" name="last_name" value="" required /><br />
+    Email:<br />
+    <input type="text" name="email" value="" required /><br />
+    Username:<br />
+    <input type="text" name="username" value="" required /><br />
+    Password:<br />
+    <input type="password" name="password" value="" required /><br />
+    Confirm Password:<br />
+    <input type="password" name="password_confirm" value="" required /><br />
+    <input type="submit" />
+  </form>
+</div>
+
+<?php include(SHARED_PATH . '/../utilities/footer.php'); ?>
